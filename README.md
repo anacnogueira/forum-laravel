c235dd4c44b8a752bdc41010f7505f6a184d7957
Ana Claudia <anacnogueira@gmail.com>
2017-11-10 12:49:46 -0200

    Testando visualização de threads

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # forum-laravel
 Forum feito com Laravel 5.5 seguindo o tutorial do Laracasts
+
+1. Thread
+2. Reply
+3. User
+
+A. Thread is created by a user
+B. A reply belongs to a thread, and belongs to a user
---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+
+    /** @text */
+    public function test_a_user_can_view_all_threads()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads');
+        $response->assertSee($thread->title);
+        
+    }
+
+    public function test_a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/'.$thread->id);
+        $response->assertSee($thread->title);
+    }
+
+
+}
