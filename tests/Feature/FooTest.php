850da87c4b7f432492923498808f30a1428ccbf5
Ana Claudia <anacnogueira@gmail.com>
2017-11-22 10:19:33 -0200

    Creating helpers to test

---
+++ tests/Feature/FooTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class classNameTest extends TestCase
+{
+    use DatabaseMigrations;
+
+     /** @test */
+    public function it_method_name()
+    {
+        
+    }
+}
+
