8bc05de5f015405d364d47e5d464b54fd16f7756
Ana Claudia <anacnogueira@gmail.com>
2017-11-28 21:30:41 -0200

    Ultimas atualizações

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -13,11 +13,14 @@ class CreateThreadsTest extends TestCase
      /** @test */
      public function guests_may_not_create_threads()
      {
-     	$this->expectException('Illuminate\Auth\AuthenticationException');
+     	
+        $this->withExceptionHandling();
 
-     	$thread = make('App\Thread');
+        $this->get('/threads/create')
+            ->assertRedirect('/login');
 
-     	$this->post('/threads', $thread->toArray());    
+        $this->post('/threads')
+            ->assertRedirect('/login');   
      }
 
     /** @test */
@@ -25,21 +28,12 @@ class CreateThreadsTest extends TestCase
     {
     	$this->signIn();
 
-    	$thread = make('App\Thread');
+    	$thread = create('App\Thread');
 
     	$this->post('/threads', $thread->toArray());    	
 
     	$response = $this->get($thread->path())
     		->assertSee($thread->title)
     		->assertSee($thread->body);
-    }
-
-    /** @test */
-    public function guests_cannot_see_the_create_thread_page()
-    {
-        $this->withExceptionHandling()
-            ->get('/threads/create')
-            ->assertRedirect('/login');
-
-    }
+    }    
 }
--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -13,18 +13,22 @@ class ParticipateInForumTest extends TestCase
 	/** @test */
 	public function unauthenticated_users_may_not_add_replies()
 	{
-		$this->expectException('Illuminate\Auth\AuthenticationException');
-
-		$this->post('/threads/1/replies', []);
+		$this->withExceptionHandling()
+			->post('/threads/channel/1/replies', [])
+			->assertRedirect('/login');
 	}
 
 	/** @test */
 	public function an_authenticated_user_may_participate_in_forum_threads()
     {
-        $this->be($user = factory('App\User')->create());
-        $thread = factory('App\Thread')->create();
-        $reply = factory('App\Reply')->make();
+        $this->signIn();
+
+
+        $thread = create('App\Thread');
+        $reply 	= make('App\Reply');
+
         $this->post($thread->path() . '/replies', $reply->toArray());
+        
         $this->get($thread->path())->assertSee($reply->body);
     }
 }
