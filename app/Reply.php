1bb19b6481059e40f12bd9c7ef344798fd0a1460
Ana Claudia <anacnogueira@gmail.com>
2017-12-22 11:33:00 -0200

    Usuarios podem favoritar respostas apenas uma vez

--- app/Reply.php
+++ app/Reply.php
@@ -12,4 +12,23 @@ class Reply extends Model
     {
     	return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function favorites()
+    {
+    	return $this->morphMany(Favorite::class, 'favorited');
+    }
+
+    public function favorite()
+    {
+    	$attributes = ['user_id' => auth()->id()];
+
+
+        if (! $this->favorites()->where($attributes)->exists()){
+            $attributes = ['user_id' => auth()->id()];
+
+            return $this->favorites()->create($attributes);
+        }
+
+        
+    }
 }
